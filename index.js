8bca42e4f8bd4ed8248d85a7abba24007fe131a6
MAAhi-Bloom <czone3144@gmail.com>
2024-11-21 12:11:36 +0600

    add 27+ images

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use("/burger", express.static(path.join(__dirname, "public/burger")));
+app.use("/pizza", express.static(path.join(__dirname, "public/pizza")));
+app.use("/tacobar", express.static(path.join(__dirname, "public/tacobar")));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
